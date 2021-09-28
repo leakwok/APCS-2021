b22f6fd6ff61d6aee2c7e89705bede05c471370f
Lea Kwok <70776123+leakwok@users.noreply.github.com>
2021-09-28 11:46:53 -0400

    Add files via upload

---
+++ pre/07/Arrays.java
@@ -0,0 +1,27 @@
+public class Arrays {
+    public static Array powArray(double[] a, int n){
+        for (int i = 0; i < a.length; i++) {
+            a[i] = Math.pow(a[i], n);
+        }
+    }
+
+    public static Array histogram(int[] a){
+        for (int i = 0; i < values.length; i++) {
+            int value = values[i];
+            System.out.println(value);
+        }
+    }
+
+    public static int indexOfMax(int[] a){
+
+    }
+
+    public static Array sieve(int n){
+        
+    }
+
+    public static void main(String[] args){
+
+    }
+
+}
