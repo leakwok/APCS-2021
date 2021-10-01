d66417ff411ad312eaf3db0b79ecc396428326d8
leakwok <lea.a.kwok@gmail.com>
2021-09-30 21:40:40 -0400

    updating file

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,8 +1,22 @@
 public class Greet {
 	public static void main(String[] args) {
-		System.out.println("No hablo queso");
+		greet("Sam");
+		greet("Pear");
+		greet("Syd");
 	}
 	
 	public static void greet(String name){
+		System.out.println("Hello " + name + "! How's your mom?");
 	}
 }
+
+/* 
+
+DISCOVERIES
+	you can invoke one method rather than writing multiple System.out.println statements in the main method
+
+
+UNRESOLVED QUESTIONS
+	was running "java Greet" in terminal only supposed to produce one hello statement with a different/random name every time?
+
+*/
