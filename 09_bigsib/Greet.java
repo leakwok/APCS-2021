bcda32c8fe9cf4b02c650c82d1abe657e8afc5a3
leakwok <lea.a.kwok@gmail.com>
2021-10-03 20:51:59 -0400

    uploading file

---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,27 @@
+/*
+Lea Kwok
+APCS
+HW09 - Adding BigSib Class
+
+
+*/
+
+public class Greet {
+	public static void main(String[] args) {
+		BigSib.greet("Sam");
+		BigSib.greet("Pear");
+		BigSib.greet("Syd");
+	}
+
+}
+
+/* 
+
+DISCOVERIES
+	you can call a method from a different file
+
+
+UNRESOLVED QUESTIONS
+	n/a
+
+*/
