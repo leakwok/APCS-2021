4692b0704f4e54f4838ec609c62b309d6f8f6628
leakwok <lea.a.kwok@gmail.com>
2021-10-03 20:52:15 -0400

    uploading file

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,24 @@
+/*
+Team Jelly: Lea Kwok, David Deng, Jaclyn Chen 
+APCS
+HW09 - Adding BigSib Class
+
+
+*/
+
+public class BigSib {
+	public static void greet(String name){
+		System.out.println("Hello " + name + "! How's your mom?");
+	}
+}
+
+/* 
+
+DISCOVERIES
+	you can call a method from a different file
+
+
+UNRESOLVED QUESTIONS
+	n/a
+
+*/
