4aa4d6e2cdd0d9e2331e5047e29e79a6463e5675
Lea Kwok <lea.a.kwok@gmail.com>
2021-10-05 02:38:38 -0400

    adding file

---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,26 @@
+/*
+Team Jelly: Lea Kwok, David Deng, Jaclyn Chen (Gunter, Nameless, Ducky)
+APCS
+HW10 - Calling a Method from a Class and Printing it
+
+
+*/
+
+public class Greet {
+    public static void main( String[] args ){
+        System.out.println( BigSib.greet("Crosby") );
+        System.out.println( BigSib.greet("Stills") );
+        System.out.println( BigSib.greet("Nash") );
+    }
+}
+
+/* 
+
+DISCOVERIES
+	you can call a method from a different file
+
+
+UNRESOLVED QUESTIONS
+	n/a
+
+*/
