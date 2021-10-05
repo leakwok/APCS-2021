e3b0eb8120dd2d5c530020f871ae31d57f9333e9
Lea Kwok <lea.a.kwok@gmail.com>
2021-10-05 02:38:21 -0400

    adding file

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,28 @@
+/*
+Team Jelly: Lea Kwok, David Deng, Jaclyn Chen (Gunter, Nameless, Ducky)
+APCS
+HW10 - Calling a Method from a Class and Printing it
+
+
+*/
+
+public class BigSib {
+	//public static void main(String [] args){
+
+	//}
+	
+	public static String greet(String name){
+		return("Hello " + name + "! How's your mom?");
+	}
+}
+
+/* 
+
+DISCOVERIES
+	you can call a method from a different file
+
+
+UNRESOLVED QUESTIONS
+	n/a
+
+*/
