a8a08daed2741d407ec4ae115ad58695adabe880
leakwok <lea.a.kwok@gmail.com>
2021-10-06 16:51:11 -0400

    uploading file

---
+++ 13_hatch/Greet.java
@@ -0,0 +1,33 @@
+/*
+Lea Kwok, Salaj Rijal (Gunter, George)
+APCS
+HW13 - Adding a Constructor
+
+*/
+
+/* 
+
+DISCO
+	you can name a variable outside of the method as long as it's in the class (thanks David)
+    you must name a global variable because you can't call a variable local to a method
+
+QCC
+	what is richard?
+    what is the purpose of richard?
+    do i even need a BigSib() even if it's in Greet.java?
+
+*/
+
+public class Greet {
+  public static void main( String[] args ) {
+    String greeting;
+
+    BigSib richard = new BigSib();
+    richard.setHelloMsg("Word up");
+
+    greeting = richard.greet("freshman");
+    System.out.println(greeting);
+
+  } //end main()
+} //end Greet
+
