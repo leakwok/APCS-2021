90643225a8132d423539970be4a6696a5b876099
Lea Kwok <lea.a.kwok@gmail.com>
2021-11-01 06:03:51 -0400

    uploading file

--- cb/string-2/doubleChar.java
+++ cb/string-2/doubleChar.java
@@ -2,12 +2,19 @@
 */
 
 public class doubleChar{
-	public String doubleChar(String str) {
-		
-	
+	public static String doubleChar(String str) {
+		String a;
+		for(int x = 0; x < str.length(); x++){
+			
+			a += str.substring(x) + str.substring(x);
+			
+		}
+		return a;
 	}
 	
 	public static void main(String args[]){
-	
+		System.out.println(doubleChar("The")); // "TThhee"
+        System.out.println(doubleChar("AAbb")); // "AAAAbbbb"
+        System.out.println(doubleChar("Hi-There")); // "HHii--TThheerree"
 	}
 }
