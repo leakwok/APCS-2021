7ab1e472f0f481ddbb548f534acd5145495451b8
Lea Kwok <lea.a.kwok@gmail.com>
2022-02-14 23:56:32 -0500

    uploading file

--- cb/recursion-1/bunnyEars.java
+++ cb/recursion-1/bunnyEars.java
@@ -1,8 +1,22 @@
-// We have a number of bunnies and each bunny has two big floppy ears. We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+/*
+lourve -- Lea Kwok and Lindsay Phung
+APCS
+HW64 -- Revisitation / codingbat recursion-1 + recursion-2
+time spent: 1 hr
+
+We have a number of bunnies and each bunny has two big floppy ears. We want to
+compute the total number of ears across all the bunnies recursively (without
+loops or multiplication).
+
+bunnyEars(0) → 0
+bunnyEars(1) → 2
+bunnyEars(2) → 4
+*/
 
 public class bunnyEars{
+
   public static int bunnyEars(int bunnies) {
-    if(bunnies == 0){
+    if(bunnies == 0) {
       return 0;
     }
     else{
@@ -10,7 +24,7 @@ public class bunnyEars{
     }
   }
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
     System.out.println(bunnyEars(0));
     System.out.println(bunnyEars(1));
     System.out.println(bunnyEars(2));
--- cb/recursion-1/factorial.java
+++ cb/recursion-1/factorial.java
@@ -1,9 +1,21 @@
-// Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1. Compute the result recursively (without loops).
+/*
+lourve -- Lea Kwok and Lindsay Phung
+APCS
+HW64 -- Revisitation / codingbat recursion-1 + recursion-2
+time spent: 1 hr
+
+Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1.
+Compute the result recursively (without loops).
+
+factorial(1) → 1
+factorial(2) → 2
+factorial(3) → 6
+*/
 
 public class factorial{
 
   public static int factorial(int n) {
-    if(n == 1){
+    if(n == 1) {
       return 1;
     }
     else {
@@ -17,9 +29,11 @@ public class factorial{
     }
     //return answer;
   }
-  public static void main(String[] args){
+
+  public static void main(String[] args) {
     System.out.println(factorial(1));
     System.out.println(factorial(2));
     System.out.println(factorial(5));
   }
+
 }
