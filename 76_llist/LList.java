791530722a75c2289e92479e18196e7241138730
Lea Kwok <lea.a.kwok@gmail.com>
2022-03-14 21:51:03 -0400

    uploading file

---
+++ 76_llist/LList.java
@@ -0,0 +1,120 @@
+/***
+ * class LList
+ * Implements a linked list of LLNodes, each containing String data
+ **/
+
+public class LList implements List //interface def must be in this dir
+{
+
+  //instance vars
+  private LLNode _head;
+  private int _size;
+
+  // constructor -- initializes instance vars
+  public LList()
+  {
+    // YOUR CODE HERE
+    _head = new LLNode("", null);
+  }
+
+
+  //--------------v  List interface methods  v--------------
+
+  public boolean add( String newVal )
+  {
+    // YOUR CODE HERE
+    _head.setCargo(newVal);
+    return true;
+  }
+
+
+  public String get( int index )
+  {
+    if ( index < 0 || index >= size() )
+      throw new IndexOutOfBoundsException();
+
+    // YOUR CODE HERE
+    LLNode temp = _head;
+
+    for(int i = 0; i < index; i++) {
+        temp = temp.getNext();
+    }
+    return temp.getCargo();
+  }
+
+
+  public String set( int index, String newVal )
+  {
+
+    if ( index < 0 || index >= size() )
+      throw new IndexOutOfBoundsException();
+
+    // YOUR CODE HERE
+    LLNode temp = _head;
+    for(int i = 0; i < index; i++) {
+        temp = temp.getNext();
+    }
+    return temp.setCargo(newVal);
+  }
+
+
+  //return number of nodes in list
+  public int size()
+  {
+    // YOUR CODE HERE
+    LLNode temp = _head;
+    int len = 0;
+    while(temp != null) {
+        len += 1;
+        temp = temp.getNext();
+    }
+    return len;
+  }
+
+  //--------------^  List interface methods  ^--------------
+
+
+
+  // override inherited toString
+  public String toString()
+  {
+    // YOUR CODE HERE
+    LLNode temp = _head;
+    String res = "";
+    while(temp != null) {
+        res += temp.getCargo();
+        res += " ";
+        temp = temp.getNext();
+    }
+    return res;
+  }
+
+
+  //main method for testing
+  public static void main( String[] args )
+  {
+    
+    LList james = new LList();
+    System.out.println( james );
+    System.out.println( "size: " + james.size() );
+    james.add("beat");
+    System.out.println( james );
+    System.out.println( "size: " + james.size() );
+    james.add("a");
+    System.out.println( james );
+    System.out.println( "size: " + james.size() );
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    james.add("need");
+    System.out.println( james );
+    System.out.println( "size: " + james.size() );
+    james.add("I");
+    System.out.println( james );
+    System.out.println( "size: " + james.size() );
+    System.out.println( "2nd item is: " + james.get(1) );
+    james.set( 1, "got" );
+    System.out.println( "...and now 2nd item is: " + james.set(1,"got") );
+    System.out.println( james );
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+  }
+
+}//end class LList
