ceeb7bec41c14a5ade7f84fa671e811f93a60d13
Lea Kwok <lea.a.kwok@gmail.com>
2022-04-06 12:42:52 -0400

    update

--- cobo/data/DataStudentCode/Welcome03_List.java
+++ cobo/data/DataStudentCode/Welcome03_List.java
@@ -9,11 +9,11 @@ import java.util.Scanner;
 public class Welcome03_List {
    public static void main(String[] args) {
       DataSource ds = DataSource.connect("http://weather.gov/xml/current_obs/index.xml").load();
-      ArrayList<WeatherStation> allstns = ds.fetchList("WeatherStation", "station/station_name", 
+      ArrayList<WeatherStation> allstns = ds.fetchList("WeatherStation", "station/station_name",
              "station/station_id", "station/state",
              "station/latitude", "station/longitude");
       System.out.println("Total stations: " + allstns.size());
-      
+
       Scanner sc = new Scanner(System.in);
       System.out.println("Enter a state abbreviation: ");
       String state = sc.next();
@@ -23,5 +23,15 @@ public class Welcome03_List {
             System.out.println("  " + ws.getId() + ": " + ws.getName());
          }
       }
+      double mostSouth = allstns.get(0).getLat();
+      int loc = 0;
+      for(int i = 0; i < allstns.size(); i++){
+        if(allstns.get(i).getLat() > mostSouth){
+          mostSouth = allstns.get(i).getLat();
+          loc = i;
+        }
+      }
+      System.out.println("The most south station is: " + allstns.get(loc).getName());
+
    }
 }
